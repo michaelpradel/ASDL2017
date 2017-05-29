53bd5b13cb11965c50a59b43945b1f2764c2da11
Michael Pradel <michael@binaervarianz.de>
2017-05-29 08:34:47 +0200

    first version

---
+++ src/code_completion_baseline.py
@@ -0,0 +1,71 @@
+import tflearn
+
+class Code_Completion_Baseline:
+
+    def token_to_string(self, token):
+        return token["type"] + "-@@-" + token["value"]
+    
+    def string_to_token(self, string):
+        splitted = string.split("-@@-")
+        return {"type": splitted[0], "value": splitted[1]}
+    
+    def one_hot(self, string):
+        vector = [0] * len(self.string_to_number)
+        vector[self.string_to_number[string]] = 1
+        return vector
+    
+    def prepare_data(self, token_lists):
+        # encode tokens into one-hot vectors
+        all_token_strings = set()
+        for token_list in token_lists:
+            for token in token_list:
+                all_token_strings.add(self.token_to_string(token))
+        print("Unique tokens: " + str(len(all_token_strings)))
+        self.string_to_number = dict()
+        self.number_to_string = dict() 
+        max_number = 0
+        for token_string in all_token_strings:
+            self.string_to_number[token_string] = max_number
+            self.number_to_string[max_number] = token_string
+            max_number += 1
+        
+        # prepare x,y pairs
+        xs = []
+        ys = []
+        for token_list in token_lists:
+            for idx, token in enumerate(token_list, start=1):
+                token_string = self.token_to_string(token)
+                previous_token_string = self.token_to_string(token_list[idx - 1])
+                xs.append(self.one_hot(previous_token_string))
+                ys.append(self.one_hot(token_string))
+
+        print("x,y pairs: " + str(len(xs)))        
+        return (xs, ys)
+
+    def create_network(self):
+        self.net = tflearn.input_data(shape=[None, len(self.string_to_number)])
+        self.net = tflearn.fully_connected(self.net, 32)
+        self.net = tflearn.fully_connected(self.net, len(self.string_to_number), activation='softmax')
+        self.net = tflearn.regression(self.net)
+        self.model = tflearn.DNN(self.net)
+    
+    def load(self, token_lists, model_file):
+        self.prepare_data(token_lists)
+        self.create_network()
+        self.model.load(model_file)
+    
+    def train(self, token_lists, model_file):
+        (xs, ys) = self.prepare_data(token_lists)
+        self.create_network()
+        self.model.fit(xs, ys, n_epoch=1, batch_size=1024, show_metric=True)
+        self.model.save(model_file)
+        
+    def query(self, prefix, suffix):
+        previous_token_string = self.token_to_string(prefix[-1])
+        x = self.one_hot(previous_token_string)
+        y = self.model.predict([x])
+        best_number = y[0].index(max(y[0]))
+        best_string = self.number_to_string[best_number]
+        best_token = self.string_to_token(best_string)
+        return [best_token]
+    
---
+++ src/runner.py
@@ -0,0 +1,85 @@
+from os import listdir
+from os.path import join, isfile
+import json
+from random import randint
+
+#########################################
+## START of part that students may change
+from code_completion_baseline import Code_Completion_Baseline
+
+training_dir = "./../../programs_800/"
+query_dir = "./../../programs_200/"
+
+model_file = "./../../trained_model"
+use_stored_model = False
+
+max_hole_size = 2
+simplify_tokens = True
+## END of part that students may change
+#########################################
+
+def simplify_token(token):
+    if token["type"] == "Identifier":
+        token["value"] = "ID"
+    elif token["type"] == "String":
+        token["value"] = "\"STR\""
+    elif token["type"] == "RegularExpression":
+        token["value"] = "/REGEXP/"
+    elif token["type"] == "Numeric":
+        token["value"] = "5"
+
+# load sequences of tokens from files
+def load_tokens(token_dir):
+    token_files = [join(token_dir, f) for f in listdir(token_dir) if isfile(join(token_dir, f)) and f.endswith("_tokens.json")]
+    token_lists = [json.load(open(f)) for f in token_files]
+    if simplify_tokens:
+        for token_list in token_lists:
+            for token in token_list:
+                simplify_token(token)
+    return token_lists
+
+# removes up to max_hole_size tokens
+def create_hole(tokens):
+    hole_size = randint(1, max_hole_size)
+    hole_start_idx = randint(1, len(tokens) - hole_size)
+    prefix = tokens[0:hole_start_idx]
+    expected = tokens[hole_start_idx:hole_start_idx + hole_size]
+    suffix = tokens[hole_start_idx + hole_size:]
+    return(prefix, expected, suffix)
+
+# checks if two sequences of tokens are identical
+def same_tokens(tokens1, tokens2):
+    if len(tokens1) != len(tokens2):
+        return False
+    for idx, t1 in enumerate(tokens1):
+        t2 = tokens2[idx]
+        if t1["type"] != t2["type"] or t1["value"] != t2["value"]:
+            return False  
+    return True
+
+#########################################
+## START of part that students may change
+code_completion = Code_Completion_Baseline()
+## END of part that students may change
+#########################################
+
+# train the network
+training_token_lists = load_tokens(training_dir)
+if use_stored_model:
+    code_completion.load(training_token_lists, model_file)
+else:
+    code_completion.train(training_token_lists, model_file)
+
+# query the network and measure its accuracy
+query_token_lists = load_tokens(query_dir)
+correct = incorrect = 0
+for tokens in query_token_lists:
+    (prefix, expected, suffix) = create_hole(tokens)
+    completion = code_completion.query(prefix, suffix)
+    if same_tokens(completion, expected):
+        correct += 1
+    else:
+        incorrect += 1
+accuracy = correct / (correct + incorrect)
+print("Accuracy: " + str(correct) + " correct vs. " + str(incorrect) + " incorrect = "  + str(accuracy))
+
