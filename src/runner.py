d6db43341b98871c6c0508b7557c250f31cc1ff9
Michael Pradel <michael@binaervarianz.de>
2017-07-20 08:53:20 +0200

    fixed bug triggered by small files and large max_hole_sizes

--- src/runner.py
+++ src/runner.py
@@ -40,7 +40,7 @@ def load_tokens(token_dir):
 
 # removes up to max_hole_size tokens
 def create_hole(tokens):
-    hole_size = randint(1, max_hole_size)
+    hole_size = min(randint(1, max_hole_size), len(tokens) - 1)
     hole_start_idx = randint(1, len(tokens) - hole_size)
     prefix = tokens[0:hole_start_idx]
     expected = tokens[hole_start_idx:hole_start_idx + hole_size]
